da1cfe7da03fc0c46d47e89826f19cb7191ac9f4
paranoid_coder <amansolankialt@gmail.com>
2025-02-09 16:02:51 +0530

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Paranoid Coder on 09/02/25.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
