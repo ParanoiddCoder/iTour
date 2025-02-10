52a748c64ff02328eee1677fc5485b7b34fa7183
paranoid_coder <amansolankialt@gmail.com>
2025-02-10 18:04:32 +0530

    All initial cache changes

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -4,18 +4,39 @@
 //
 //  Created by Paranoid Coder on 09/02/25.
 //
-
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    
+    @Query var destinations: [Destination]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List{
+                ForEach(destinations){ destination in
+                    VStack(alignment: .leading){
+                        Text(destination.name)
+                            .font(.headline)
+                        
+                        Text(destination.date.formatted(date: .long, time: .shortened))
+                    }
+                }
+            }
+            .navigationTitle("iTour")
+            .toolbar{
+                Button("Add Samples", action: addSamples)
+            }
         }
-        .padding()
+    }
+    func addSamples() {
+        let welligton = Destination( name: "Welligton")
+        let sydney = Destination( name: "Sydney")
+        let paris = Destination( name: "Paris")
+        
+        modelContext.insert(welligton)
+        modelContext.insert(sydney)
+        modelContext.insert(paris)
     }
 }
 
